e42d0ed76cb3a9edc9eadcd24301b23cdd07d648
andreystepan <92333697+andreystepan@users.noreply.github.com>
2022-09-20 00:34:30 +0300

    add Filter

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import { ContactList } from 'Contacts/ContactList/ContactList';
 import { ContactForm } from './ContactForm/ContactForm';
+import { Filter } from './Filter/Filter';
 
 export const App = () => {
   return (
     <>
       <ContactForm />
+      <Filter />
       <ContactList />
     </>
   );
